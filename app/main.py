9b89d3767a3ccd698544cdb9e749207b25ee0c4b
geovanni <geovanni.zucolli@escola.pr.gov.br>
2026-04-27 20:04:57 -0300

    update-001-register

--- app/main.py
+++ app/main.py
@@ -1,7 +1,36 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import os
+from dotenv import load_dotenv
+from pwdlib import PasswordHash
+from time import sleep
+
+load_dotenv()
+
+db_host = os.getenv("DB_HOST")
+db_user = os.getenv("DB_USER")
+db_pass = os.getenv("DB_PASS")
+db_name = os.getenv("DB_NAME")
 
 app = FastAPI()
+
+password_helper = PasswordHash.recommended()
+
+while True:
+
+    try:
+        conn = psycopg2.connect(host=db_host, database=db_name,
+        user = db_user, password=db_pass, cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("Database conection was sucessful")
+        break
+    except Exception as error:
+        print("Connecting to database failed")
+        print("Error: ", error)
+        sleep(3)
+
 
 from app.routes import auth_router
 
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,33 @@
-from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse
+from .main import cursor,conn, password_helper
+from typing import Annotated
+from fastapi import APIRouter, Request, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 
 
 templates = Jinja2Templates(directory="templates")
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
-@auth_router.get("/login", response_class=HTMLResponse)
+@auth_router.get("/register", response_class=HTMLResponse)
 async def read_item(request: Request):
     return templates.TemplateResponse(
-        request=request, name="item.html"
+        request,
+        "item.html"
     )
+
+@auth_router.post("/register")
+def create_user(
+    name: Annotated[str, Form()],
+    email: Annotated[str, Form()],
+    password: Annotated[str, Form()]):
+    try:
+        hashed_password = password_helper.hash(password)
+        cursor.execute("""INSERT INTO players (name, email, password) VALUES (%s, %s, %s) RETURNING * """, (name, email, hashed_password))
+
+        new_player = cursor.fetchone()
+
+        conn.commit()
+
+        return RedirectResponse(url="/auth/login", status_code=303)
+    except TypeError as e:
+        return{"error": f"Error {e}"}
